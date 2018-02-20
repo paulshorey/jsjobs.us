a8958ca53833107735eef785e577b84e2b965bd5
paul (local) <ps@artspaces.net>
2018-02-19 17:46:22 -0800

    initial commit - next.js bootstrapped

---
+++ components/nav.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+export default class Nav extends React.Component {
+  render () {
+    return (
+      <div>
+        <img src="/static/images/banner.jpg" />
+      </div>
+    )
+  }
+}
