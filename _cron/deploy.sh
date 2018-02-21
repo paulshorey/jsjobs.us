63a60bbc9dd54d7a5bc1cc271634791b8e4d6d0d
paul (local) <ps@artspaces.net>
2018-02-21 12:50:36 -0800

    fix deploy.sh

--- _cron/deploy.sh
+++ _cron/deploy.sh
@@ -1,5 +1,7 @@
 sleep 20;
 
+cd /www/ps-jobs
+
 # mkdir ./api_public/v1/console/logfiles;
 # pm2 start _deploy.js -o ./api_public/v1/console/logfiles/_deploy_log.log -e ./api_public/v1/console/logfiles/_deploy_err.log;
 pm2 start _deploy.js
