86506b8309d78e32ffc0d8a9cb36e0f55da9e029
paul (local) <ps@artspaces.net>
2018-02-21 14:03:41 -0800

    enable iptables PREROUTING 80 to 3000

--- _cron/app.sh
+++ _cron/app.sh
@@ -4,8 +4,8 @@
 
 
 # start app
-# iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 1080
-# ufw allow 80/tcp
+iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
+ufw allow 80/tcp
 # iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 1443
 # ufw allow 443/tcp
 
