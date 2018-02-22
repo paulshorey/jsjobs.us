fde7c9e8d6b816794eb53090e04743c181efb100
paul (local) <ps@artspaces.net>
2018-02-21 19:22:49 -0800

    try without prefetch

--- components/nav.js
+++ components/nav.js
@@ -8,12 +8,12 @@ export default class Nav extends React.Component {
       <div>
         <img src="/static/images/banner.jpg" />
         <p>
-          <div><Link prefetch href="/"><a>&laquo; go home</a></Link></div>
-          <div><Link prefetch href='/search/react'><a>search "react" &raquo;</a></Link></div>
-          <div><Link prefetch href='/search/node.js'><a>search "node.js" &raquo;</a></Link></div>
-          <div><Link prefetch href='/search/whatever'><a>search "whatever" &raquo;</a></Link></div>
-          <div><Link prefetch href='/adsfdfasdf'><a>asdffdfasdf &raquo;</a></Link></div>
-          <div><Link prefetch href='/about'><a>about &raquo;</a></Link></div>
+          <div><Link href="/"><a>&laquo; go home</a></Link></div>
+          <div><Link href='/search/react'><a>search "react" &raquo;</a></Link></div>
+          <div><Link href='/search/node.js'><a>search "node.js" &raquo;</a></Link></div>
+          <div><Link href='/search/whatever'><a>search "whatever" &raquo;</a></Link></div>
+          <div><Link href='/adsfdfasdf'><a>asdffdfasdf &raquo;</a></Link></div>
+          <div><Link href='/about'><a>about &raquo;</a></Link></div>
         </p>
       </div>
     )
