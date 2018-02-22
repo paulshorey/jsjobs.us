1af9b6be6bf5f3fd4bef97a298b703304d3e7fed
paul (local) <ps@artspaces.net>
2018-02-21 18:39:36 -0800

    idk root url not linking properly in app

--- components/nav.js
+++ components/nav.js
@@ -8,7 +8,7 @@ export default class Nav extends React.Component {
       <div>
         <img src="/static/images/banner.jpg" />
         <p>
-          <div><Link href='/'><a>&laquo; go home</a></Link></div>
+          <div><Link prefetch href="/index" as "/"><a>&laquo; go home</a></Link></div>
           <div><Link prefetch href='/search/react'><a>search "react" &raquo;</a></Link></div>
           <div><Link prefetch href='/search/node.js'><a>search "node.js" &raquo;</a></Link></div>
           <div><Link prefetch href='/search/whatever'><a>search "whatever" &raquo;</a></Link></div>
