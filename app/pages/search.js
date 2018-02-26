5cdbea4adb79b75002e8d083855def1c99a40ba5
paul (local) <ps@artspaces.net>
2018-02-25 18:58:51 -0800

    /api/v1

--- app/pages/search.js
+++ app/pages/search.js
@@ -7,7 +7,7 @@ export default class MyPage extends React.Component {
     static async getInitialProps (params) {
         // console.log("\search getInitialProps", params, "\n");
         // eslint-disable-next-line no-undef
-        const res = await fetch('http://localhost:1080/v1/jobs/all?text='+(params.query.search||''))
+        const res = await fetch('http://localhost/api/v1/jobs/all?text='+(params.query.search||''))
         const json = await res.json()
         return { jobs_count:json.results, jobs:json.data };
     }
