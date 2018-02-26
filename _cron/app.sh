b4e8d793e1f31161e98d7b71a43b1f1dbbad7054
paul (local) <ps@artspaces.net>
2018-02-26 15:45:49 -0700

    undo iptables

--- _cron/app.sh
+++ _cron/app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
-ufw allow 80/tcp
+# iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
+# ufw allow 80/tcp
 # iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 1443
 # ufw allow 443/tcp
 
