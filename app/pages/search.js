3df20c69610d3ae0c82f1d072ad0d184bdc97d12
paul (local) <ps@artspaces.net>
2018-02-25 23:38:05 -0800

    figured out hostname finally

--- app/pages/search.js
+++ app/pages/search.js
@@ -3,48 +3,44 @@ import 'isomorphic-unfetch'
 import Nav from 'components/nav'
 
 const getHostname = function(){
-  console.log( '(typeof window) ...' + typeof window );
-  console.log( '(typeof window === "undefined") ...' + (typeof window === "undefined") );
-  // if (typeof window !== "undefined" && window.location) {
-  //   console.log(window.location.protocol+"//"+window.location.hostname);
-  //   return window.location.protocol+"//"+window.location.hostname;
-  // } else {
-  //   console.log('http://localhost');
-  //   return "http://localhost";
-  // }
+	if (typeof window === "undefined") {
+		console.log('lets try localhost');
+		return "http://localhost";
+	} else {
+		console.log(window.location.protocol+"//"+window.location.hostname);
+		return window.location.protocol+"//"+window.location.hostname;
+	}
 };
 
 export default class MyPage extends React.Component {
-    static async getInitialProps (params) {
-        // console.log("\search getInitialProps", params, "\n");
-        // eslint-disable-next-line no-undef
-        // const hostname = 'jobs.paulshorey.com';
-        const res = await fetch('https://jobs.paulshorey.com/api/v1/jobs?text='+(params.query.search||''))
-        const json = await res.json()
-        return { jobs_count:json.results, jobs:json.data };
-    }
+	static async getInitialProps (params) {
+		// eslint-disable-next-line no-undef
+		const res = await fetch(getHostname()+'/api/v1/jobs?text='+(params.query.search||''))
+		const json = await res.json()
+		return { jobs_count:json.results, jobs:json.data };
+	}
   
   render () {
-    // console.log("\search render", this.props, "\n");
-    var Jobs = [];
-    if (this.props && this.props.jobs) {
-      this.props.jobs.forEach(function(job,i){
-        Jobs.push(
-          <div key={job._id}>
-            <div>{job.text}</div>
-            <sup>{job.location}</sup>
-          </div>
-        );
-      });
-    }
-    return (
-        <div>
-          <Nav />
-          <h2>Search "{this.props.url.query.search}" results: {this.props.jobs_count}</h2>
-          <div>
-            {Jobs}
-          </div>
-        </div>
-    )
+	// console.log("\search render", this.props, "\n");
+	var Jobs = [];
+	if (this.props && this.props.jobs) {
+	  this.props.jobs.forEach(function(job,i){
+		Jobs.push(
+		  <div key={job._id}>
+			<div>{job.text}</div>
+			<sup>{job.location}</sup>
+		  </div>
+		);
+	  });
+	}
+	return (
+		<div>
+		  <Nav />
+		  <h2>Search "{this.props.url.query.search}" results: {this.props.jobs_count}</h2>
+		  <div>
+			{Jobs}
+		  </div>
+		</div>
+	)
   }
 }
