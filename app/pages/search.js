361d1ed9765da9fed05eba3c743497d2fe38b430
paul (local) <ps@artspaces.net>
2018-02-25 23:22:43 -0800

    just use absolute hostname

--- app/pages/search.js
+++ app/pages/search.js
@@ -2,23 +2,12 @@ import React from 'react'
 import 'isomorphic-unfetch'
 import Nav from 'components/nav'
 
-const getHostname = function(){
-  console.log('getHostname() ...');
-  if (typeof window !== "undefined" && window.location) {
-    console.log(window.location.protocol+"//"+window.location.hostname);
-    return window.location.protocol+"//"+window.location.hostname;
-  } else {
-    console.log('http://localhost');
-    return "http://localhost";
-  }
-};
-
 export default class MyPage extends React.Component {
     static async getInitialProps (params) {
         // console.log("\search getInitialProps", params, "\n");
         // eslint-disable-next-line no-undef
         // const hostname = 'jobs.paulshorey.com';
-        const res = await fetch(getHostname()+'/api/v1/jobs?text='+(params.query.search||''))
+        const res = await fetch('https://jobs.paulshorey.com/api/v1/jobs?text='+(params.query.search||''))
         const json = await res.json()
         return { jobs_count:json.results, jobs:json.data };
     }
